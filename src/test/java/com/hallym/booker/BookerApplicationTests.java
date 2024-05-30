ed1f5ded023b9f565c463ae1732b8f1a513d69df
choijimin <jiminchoi1221@gmail.com>
2024-05-30 22:47:46 +0900

    start: booker backend v4

---
+++ src/test/java/com/hallym/booker/BookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hallym.booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
