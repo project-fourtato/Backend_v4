3886a13588f332c755145b6b010855095f6f5d9b
yeeun <wjdwlr03@gmail.com>
2024-06-25 16:06:01 +0900

    FOUR-33 Feat : userBooks repository 생성

--- src/main/java/com/hallym/booker/repository/UserBooksRepository.java
+++ src/main/java/com/hallym/booker/repository/UserBooksRepository.java
@@ -1,7 +1,36 @@
 package com.hallym.booker.repository;
 
+import com.hallym.booker.domain.Profile;
 import com.hallym.booker.domain.UserBooks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserBooksRepository extends JpaRepository<UserBooks, Long> {
+
+    //특정 프로필의 모든 책 조회
+    @Query("SELECT ub FROM UserBooks ub WHERE ub.profile = :profile")
+    List<UserBooks> findAllByProfile(@Param("profile") Profile profile);
+
+    // 특정 프로필의 모든 책 읽기 상태 조회
+    @Query("SELECT ub.readStatus FROM UserBooks ub WHERE ub.profile = :profile")
+    List<Integer> findReadStatusByProfile(@Param("profile") Profile profile);
+
+    //특정 프로필의 모든 책 판매 상태 조회
+    @Query("SELECT ub.saleStatus FROM UserBooks ub WHERE ub.profile = :profile")
+    List<Integer> findSaleStatusByProfile(@Param("profile") Profile profile);
+
+    // isbn으로 해당 책을 가진 모든 프로필 조회
+    @Query("SELECT ub.profile FROM UserBooks ub WHERE ub.bookDetails.isbn = :isbn")
+    List<Profile> findAllProfilesByIsbn(@Param("isbn") String isbn);
+
+    // 책 검색에서 ub.profile과 ISBN으로 책 읽기 상태 조회
+    @Query("SELECT ub FROM UserBooks ub WHERE ub.bookDetails.isbn = :isbn AND ub.profile = :profile")
+    UserBooks findByProfileUidAndIsbn(@Param("profile") Profile profile, @Param("isbn") String isbn);
+
+    // 주어진 ISBN과 판매 상태가 활성화된 책을 가진 프로필들을 조회
+    @Query("SELECT ub.profile FROM UserBooks ub WHERE ub.bookDetails.isbn = :isbn AND ub.saleStatus = 1")
+    List<Profile> findByIsbnAndSalesstate(@Param("isbn") String isbn);
 }
