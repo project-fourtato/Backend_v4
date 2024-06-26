8a12f95e2bd72797e3c54b29137a48107ea485f0
choijimin <jiminchoi1221@gmail.com>
2024-06-26 17:38:42 +0900

    FOUR-32 Feat: DirectmessageRepository, FollowRepository 생성

---
+++ src/main/java/com/hallym/booker/repository/DirectmessageRepository.java
@@ -0,0 +1,18 @@
+package com.hallym.booker.repository;
+
+import com.hallym.booker.domain.Directmessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface DirectmessageRepository extends JpaRepository<Directmessage, Long> {
+
+    @Query(value = "SELECT m FROM Directmessage m WHERE m.profile.profileUid = :sender_uid")
+    List<Directmessage> findAllDirectmessagesBySender(@Param("sender_uid") Long senderUid);
+
+    @Query(value = "SELECT m FROM Directmessage m WHERE m.recipientUid = :recipient_uid")
+    List<Directmessage> findAllDirectMessagesByRecipient(@Param("recipient_uid") Long recipientUid);
+
+}
