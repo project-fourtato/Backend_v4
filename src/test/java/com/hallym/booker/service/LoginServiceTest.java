b4a65c1569b6983c7ba18398c21b6111fc155265
jangdayeon <dayeonjang.dev@gmail.com>
2024-06-28 01:29:57 +0900

    FOUR-35 Test : LoginService Test

---
+++ src/test/java/com/hallym/booker/service/LoginServiceTest.java
@@ -0,0 +1,95 @@
+package com.hallym.booker.service;
+
+import com.hallym.booker.domain.Login;
+import com.hallym.booker.repository.LoginRepository;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@RunWith(SpringRunner.class)
+class LoginServiceTest {
+    @Autowired
+    LoginService loginService;
+    @Autowired
+    LoginRepository loginRepository;
+    @BeforeEach
+    void setUp() {
+        //Given
+        Date now = new Date();
+        Login login1 = Login.create("id1","pw1","email1",now);
+        login1 = loginRepository.save(login1);
+        Login login2 = Login.create("id2","pw2","email2",now);
+        login2 = loginRepository.save(login2);
+    }
+
+    @Test
+    void join() {
+        Date now = new Date();
+        Login new_login = Login.create("newId","newPw","newEmail",now);
+        loginService.join(new_login);
+        Login check = loginRepository.findById(new_login.getLoginUid()).get();
+        Assertions.assertThat(new_login.getLoginUid()).isEqualTo(check.getLoginUid());
+    }
+
+    @Test
+    void 이미등록된회원일경우(){
+        Date now = new Date();
+        Login new_login = Login.create("id1","newPw","newEmail",now);
+        assertThrows(IllegalStateException.class,
+                ()-> loginService.join(new_login));
+    }
+
+    @Test
+    void checkId() {
+        Boolean result1 = loginService.checkId("id1");
+        Assertions.assertThat(result1).isEqualTo(false);
+        Boolean result2 = loginService.checkId("newId");
+        Assertions.assertThat(result2).isEqualTo(true);
+
+    }
+
+    @Test
+    void findOne() {
+        Login login = loginService.findOne("id1");
+        Assertions.assertThat(login.getPw()).isEqualTo("pw1");
+    }
+
+    @Test
+    void deleteOne() {
+        loginService.deleteOne("id1");
+        assertThrows(NoSuchElementException.class, ()->
+                loginService.findOne("id1"));
+    }
+
+    @Test
+    void updateLogin() {
+        Login login = loginService.findOne("id1");
+        loginService.updateLogin(login.getLoginUid(),"changePw","changeEmail",login.getBirth());
+        Login changeLogin = loginService.findOne("id1");
+        Assertions.assertThat("changePw").isEqualTo(changeLogin.getPw());
+    }
+
+    @Test
+    void loginLogin() {
+        Login login = loginService.loginLogin("id1","pw1");
+        Assertions.assertThat(login.getLoginUid()).isEqualTo("id1");
+    }
+
+    @Test
+    void 로그인실패(){
+        Login login = loginService.loginLogin("id1","pw2");
+        Assertions.assertThat(login).isEqualTo(null);
+    }
+}
