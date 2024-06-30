49f5b6703af3d95b97280d0740ad687211116b18
jangdayeon <dayeonjang.dev@gmail.com>
2024-06-30 14:18:49 +0900

    Revert "FOUR-38 Feat : Follow 삭제 후 Followings 엔티티 추가"
    This reverts commit 2d1b1a6f7595966cc6d59d3d2ec72505e15fd8e5.

--- src/main/java/com/hallym/booker/domain/Follow.java
+++ src/main/java/com/hallym/booker/domain/Follow.java
@@ -9,32 +9,32 @@ import static jakarta.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Followings {
+public class Follow {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long followingsId;
-    private Long followingUid;
+    private Long followId;
+    private Long toUserId;
 
     // 외래키 참조
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name= "profile_uid")
+    @JoinColumn(name= "from_user_id")
     private Profile profile;
 
-    private Followings(Long followingUid) {
-        this.followingUid = followingUid;
+    private Follow(Long toUserId) {
+        this.toUserId = toUserId;
     }
 
     // 생성 메서드
-    public static Followings create(Profile profile, Long followingUid){
-        Followings followings = new Followings(followingUid);
-        followings.addProfile(profile);
-        return followings;
+    public static Follow create(Profile profile, Long toUserId){
+        Follow follows = new Follow(toUserId);
+        follows.addProfile(profile);
+        return follows;
     }
 
     //연관관계 편의 메서드
     private void addProfile(Profile profile){
         this.profile = profile;
-        profile.getFollowings().add(this);
+        profile.getFollow().add(this);
     }
 
 }
