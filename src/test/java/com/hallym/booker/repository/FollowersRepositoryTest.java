cb7ef3e94aad76ad7114f4ef39e5fe9316e64829
jangdayeon <dayeonjang.dev@gmail.com>
2024-06-30 12:19:33 +0900

    FOUR-38 Test : FollowersRepositoryTest 추가

--- src/test/java/com/hallym/booker/repository/FollowersRepositoryTest.java
+++ src/test/java/com/hallym/booker/repository/FollowersRepositoryTest.java
@@ -1,10 +1,9 @@
 package com.hallym.booker.repository;
 
-import com.hallym.booker.domain.Followings;
+import com.hallym.booker.domain.Followers;
 import com.hallym.booker.domain.Login;
 import com.hallym.booker.domain.Profile;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +15,21 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
-public class FollowingsRepositoryTest {
+public class FollowersRepositoryTest {
     @Autowired
-    FollowingsRepository followingsRepository;
+    FollowersRepository followersRepository;
     @Autowired
     LoginRepository loginRepository;
     @Autowired
     ProfileRepository profileRepository;
 
     @Test
-    public void findByProfile_ProfileUidAndAndFollowingUid() {
+    public void findByProfile_ProfileUidAndAndFollowerUid() {
         //Given
         Login loginA = Login.create("gamja", "gamgamja", "gam@gamja.com", new Date(2000, 12, 21));
         Login userALogin = loginRepository.save(loginA);
@@ -42,11 +41,11 @@ public class FollowingsRepositoryTest {
         Profile profileB = Profile.create(userBLogin, "gogumaa", "https://gogumasprofileimage.com", "gogumasprofileimage", "무력 고구마");
         Profile userProfileB = profileRepository.save(profileB);
 
-        Followings followings = Followings.create(userProfileA, userProfileB.getProfileUid());
-        Followings followingsUser = followingsRepository.save(followings);
+        Followers followers = Followers.create(userProfileA, userProfileB.getProfileUid());
+        Followers followersUser = followersRepository.save(followers);
 
         //when
-        Optional<Followings> byFromUserIdAndToUserId = followingsRepository.findByProfile_ProfileUidAndAndFollowingUid(followingsUser.getProfile().getProfileUid(), followingsUser.getFollowingUid());
+        Optional<Followers> byFromUserIdAndToUserId = followersRepository.findByProfile_ProfileUidAndAndFollowerUid(followersUser.getProfile().getProfileUid(), followersUser.getFollowerUid());
 
         //then
         if(byFromUserIdAndToUserId.isPresent()) {
@@ -57,7 +56,7 @@ public class FollowingsRepositoryTest {
     }
 
     @Test
-    @DisplayName("followings 조회, 객체 카운팅 쿼리 테스트")
+    @DisplayName("followers 조회, 객체 카운팅 쿼리 테스트")
     public void findByProfile_ProfileUidAndCountTest() {
         Login loginA = Login.create("gamja", "gamgamja", "gam@gamja.com", new Date(2000, 12, 21));
         Login userALogin = loginRepository.save(loginA);
@@ -74,18 +73,17 @@ public class FollowingsRepositoryTest {
         Profile profileC = Profile.create(userCLogin, "tofu", "https://tofusprofileimage.com", "tofusprofileimage", "무력 두부");
         Profile userProfileC = profileRepository.save(profileC);
 
-        Followings followingsGamjaGoguma = Followings.create(userProfileA, userProfileB.getProfileUid());
-        Followings gamjaToGoguma = followingsRepository.save(followingsGamjaGoguma);
-        Followings followingsGamjaTofu = Followings.create(userProfileA, userProfileC.getProfileUid());
-        Followings gamjaToTofu = followingsRepository.save(followingsGamjaTofu);
+        Followers followersGamjaGoguma = Followers.create(userProfileA, userProfileB.getProfileUid());
+        Followers gamjaToGoguma = followersRepository.save(followersGamjaGoguma);
+        Followers followersGamjaTofu = Followers.create(userProfileA, userProfileC.getProfileUid());
+        Followers gamjaToTofu = followersRepository.save(followersGamjaTofu);
 
         //when
-        List<Followings> followings = followingsRepository.findByProfile_ProfileUid(userProfileA.getProfileUid());
-        Long cnt = followingsRepository.countByProfile_ProfileUid(userProfileA.getProfileUid());
+        List<Followers> followers = followersRepository.findByProfile_ProfileUid(userProfileA.getProfileUid());
+        Long cnt = followersRepository.countByProfile_ProfileUid(userProfileA.getProfileUid());
 
         //then
-        assertThat(followings).extracting(Followings::getFollowingUid).contains(gamjaToGoguma.getFollowingUid(), gamjaToTofu.getFollowingUid());
+        assertThat(followers).extracting(Followers::getFollowerUid).contains(gamjaToGoguma.getFollowerUid(), gamjaToTofu.getFollowerUid());
         assertThat(cnt).isEqualTo(2);
     }
-
 }
