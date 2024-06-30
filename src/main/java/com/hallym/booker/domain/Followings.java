2d1b1a6f7595966cc6d59d3d2ec72505e15fd8e5
jangdayeon <dayeonjang.dev@gmail.com>
2024-06-30 11:56:55 +0900

    FOUR-38 Feat : Follow 삭제 후 Followings 엔티티 추가

--- src/main/java/com/hallym/booker/domain/Followings.java
+++ src/main/java/com/hallym/booker/domain/Followings.java
@@ -9,32 +9,32 @@ import static jakarta.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Follow {
+public class Followings {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long followId;
-    private Long toUserId;
+    private Long followingsId;
+    private Long followingUid;
 
     // 외래키 참조
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name= "from_user_id")
+    @JoinColumn(name= "profile_uid")
     private Profile profile;
 
-    private Follow(Long toUserId) {
-        this.toUserId = toUserId;
+    private Followings(Long followingUid) {
+        this.followingUid = followingUid;
     }
 
     // 생성 메서드
-    public static Follow create(Profile profile, Long toUserId){
-        Follow follows = new Follow(toUserId);
-        follows.addProfile(profile);
-        return follows;
+    public static Followings create(Profile profile, Long followingUid){
+        Followings followings = new Followings(followingUid);
+        followings.addProfile(profile);
+        return followings;
     }
 
     //연관관계 편의 메서드
     private void addProfile(Profile profile){
         this.profile = profile;
-        profile.getFollow().add(this);
+        profile.getFollowings().add(this);
     }
 
 }
