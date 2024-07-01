6bd902de6bfa9ba262630742ae17fe5a0aaeafae
choijimin <jiminchoi1221@gmail.com>
2024-07-01 21:31:49 +0900

    FOUR-7 Feat: S3 upload 기능 구현

--- src/main/java/com/hallym/booker/global/S3/S3Controller.java
+++ src/main/java/com/hallym/booker/global/S3/S3Controller.java
@@ -1,2 +1,25 @@
-package com.hallym.booker.global.S3;public class S3Controller {
+package com.hallym.booker.global.S3;
+
+import com.hallym.booker.global.S3.dto.S3ResponseUploadEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class S3Controller {
+
+    private final S3Service s3service;
+
+    @PostMapping("/S3/upload")
+    public S3ResponseUploadEntity upload(@RequestParam("file")MultipartFile file, String dir) throws Exception {
+        if(file.isEmpty()) {
+            return new S3ResponseUploadEntity("file is empty", null);
+        }
+
+        return s3service.upload(file, dir);
+    }
 }
--- src/main/java/com/hallym/booker/global/S3/S3Service.java
+++ src/main/java/com/hallym/booker/global/S3/S3Service.java
@@ -1,8 +1,57 @@
 package com.hallym.booker.global.S3;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.hallym.booker.global.S3.dto.S3ResponseUploadEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController
-public class S3Controller {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class S3Service {
+    private final AmazonS3 amazonS3;
+    private final String bucket;
+
+    public S3Service(AmazonS3 amazonS3, @Value("${cloud.aws.s3.bucket}") String bucket) {
+        this.amazonS3 = amazonS3;
+        this.bucket = bucket;
+    }
+
+    public S3ResponseUploadEntity upload(MultipartFile multipartFile, String dir) throws Exception {
+        String fileName = multipartFile.getOriginalFilename();
+
+        String uuid = UUID.randomUUID().toString();
+        String uniqueFileName = dir + "/" + uuid + "-" + fileName.replace("//s", "-");
+        String url = "https://booker-v4-bucket.s3.amazonaws.com/";
+
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucket,
+                uniqueFileName,
+                multipartFile.getInputStream(),
+                getObjectMetadata(multipartFile));
+
+        try {
+            amazonS3.putObject(putObjectRequest);
+        } catch (AmazonServiceException e) {
+            log.error(e.getErrorMessage());
+        }
+
+        return new S3ResponseUploadEntity(uniqueFileName, url + uniqueFileName);
+    }
+
+    public ObjectMetadata getObjectMetadata(MultipartFile file) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        return metadata;
+    }
 }
