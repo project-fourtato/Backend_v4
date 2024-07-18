4859a5dcbf23ef8909518ac3ca237df8c389ac7b
yeeun <wjdwlr03@gmail.com>
2024-07-18 21:28:41 +0900

    FOUR-78 Test : Interests Test 코드 추가

---
+++ src/test/java/com/hallym/booker/service/InterestsServiceTest.java
@@ -0,0 +1,60 @@
+package com.hallym.booker.service;
+
+import com.hallym.booker.domain.Interests;
+import com.hallym.booker.dto.Interests.InterestsResponseDTO;
+import com.hallym.booker.repository.InterestsRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class InterestsServiceTest {
+
+    @Mock
+    private InterestsRepository interestsRepository;
+
+    @InjectMocks
+    private InterestsServiceImpl interestsService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getInterestsByProfile() {
+        // Given
+        Long profileUid = 1L;
+        Interests interest = mock(Interests.class);
+        when(interestsRepository.findByProfile_ProfileUid(profileUid)).thenReturn(List.of(interest));
+
+        // When
+        List<InterestsResponseDTO> result = interestsService.getInterestsByProfile(profileUid);
+
+        // Then
+        assertThat(result).isNotEmpty();
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    void getAllInterestsExceptProfile() {
+        // Given
+        Long profileUid = 1L;
+        Interests interest = mock(Interests.class);
+        when(interestsRepository.findByProfile_ProfileUidNotIn(profileUid)).thenReturn(List.of(interest));
+
+        // When
+        List<InterestsResponseDTO> result = interestsService.getAllInterestsExceptProfile(profileUid);
+
+        // Then
+        assertThat(result).isNotEmpty();
+        assertThat(result.size()).isEqualTo(1);
+    }
+}
