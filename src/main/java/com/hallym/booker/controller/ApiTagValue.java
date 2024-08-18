a999c039692166dbf07c57a010ac23612540429b
choijimin <jiminchoi1221@gmail.com>
2024-08-18 16:19:32 +0900

    FOUR-122 Test: git actions Test

--- src/main/java/com/hallym/booker/controller/ApiTagValue.java
+++ src/main/java/com/hallym/booker/controller/ApiTagValue.java
@@ -6,7 +6,7 @@ import org.w3c.dom.NodeList;
 
 @Component
 public class ApiTagValue {
-    // test
+    // test testtest
     // tag값의 정보를 가져오는 함수
     public String getTagValue(String tag, Element eElement) {
         NodeList nlList = eElement.getElementsByTagName(tag);
